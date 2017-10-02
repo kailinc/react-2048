47e9e97016fc7c2e98d9d0984da91a254c9f27c0
kailinc <kailinc@bu.edu>
2017-10-02 10:42:06 -0400

    Created Board Component
    - render the current state of the board
    - easy to implement

---
+++ src/Board.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Board extends Component {
+  render() {
+    return(
+      <div>
+        <p>This is Board</p>
+      </div>
+    )
+  }
+}
+
+export default Board;
--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Score from './Score';
+import Board from './Board';
 
 class Game extends Component {
   constructor() {
@@ -13,9 +14,9 @@ class Game extends Component {
   render() {
     return(
       <div>
-        <p>This is Game Component</p>
         <Score type="Current" score={this.state.curScore}/>
         <Score type="High" score={this.state.highScore}/>
+        <Board/>
       </div>
     )
   }
