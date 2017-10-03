cb9bc47c360b385e93c278c5f3f275f086b93afa
kailinc <kailinc@bu.edu>
2017-10-03 09:49:24 -0400

    Organized Render() of Board
    - organized board to render by rows
    - updated board state to reflex its index to test order of components
    - need to do css

--- src/Board.js
+++ src/Board.js
@@ -10,8 +10,29 @@ class Board extends Component {
     return(
       <div>
         <p>This is Board</p>
-        <div>
+        <div className="row">
           {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+        </div>
+        <div className="row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+        </div>
+        <div className="row">
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+          {this.renderSquare(10)}
+          {this.renderSquare(11)}
+        </div>
+        <div className="row">
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
+          {this.renderSquare(14)}
+          {this.renderSquare(15)}
         </div>
       </div>
     )
--- src/Game.js
+++ src/Game.js
@@ -6,7 +6,7 @@ class Game extends Component {
   constructor() {
     super()
     this.state = {
-      board: [0,0,2,0,4,8,16,0,2,2,2,0,2,2,0,2],
+      board: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
       curScore: 0,
       highScore: 1
     }
