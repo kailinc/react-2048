6fbf75eb3ab2e5cb80f0830ab0725272aa1ab513
kailinc <kailinc@bu.edu>
2017-10-03 09:36:14 -0400

    Created Square Component
    - Square will render value of each spot on the board
    - will have animation (ComponentWillMount) i think

--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Square from './Square';
+
 class Board extends Component {
   render() {
     return(
       <div>
         <p>This is Board</p>
+        <div>
+          <Square />
+        </div>
       </div>
     )
   }
---
+++ src/Square.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+  render() {
+    return(
+      <div>
+        This is Square
+      </div>
+    )
+  }
+}
+
+export default Square;
