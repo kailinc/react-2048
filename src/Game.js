42dbef6b26bde862fe7ebf571125090e35470e43
kailinc <kailinc@bu.edu>
2017-10-18 11:12:20 -0400

    Updated newBoard()
    - now it will randomly assign 2 to 2 indexs in the board
    - I am very happy with this approach
    - I have to create a function 2or4() that will randomly return 2 or 4
      with more weight in 2

--- src/Game.js
+++ src/Game.js
@@ -207,11 +207,20 @@ const verFormatBoard = function (board) {
 
 // returns a new board with 2 items (2 or 4) at random places
 const newBoard = function () {
-  let emptyArray = new Array(16).fill(0)
+  let board = new Array(16).fill(0)
   // ranIndex1 =  random number between 0 and 15
-  // randIndex2 =  random number between 0 and 15 that is not the first number
+  // randIndex2 =  random number between 0 and 15 and is not the first number
   // assign emptyArray[ranIndex1] = randomNumer between 2 and 4
   // assign emptyArray[ranIndex2] = randomNumer between 2 and 4
+  let ranIndex1 = Math.floor(Math.random() * 16)
+  let ranIndex2 = Math.floor(Math.random() * 16)
+  // if the two indx are the same assign randIndex2 a new random number until they don't have the same one
+  while (ranIndex1 === ranIndex2) {
+    ranIndex2 = Math.floor(Math.random() * 16)
+  }
+  board[ranIndex1] = 2
+  board[ranIndex2] = 2
+  return board
 }
 
 export default Game;
