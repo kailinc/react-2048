689ba55a298628a78838bab915db66468757ba86
kailinc <kailinc@bu.edu>
2017-11-13 14:50:23 -0500

    Added Rendering for new scores
    - used map to loop through this.scoresToAdd

--- src/Score.js
+++ src/Score.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react'
 import './styles/index.css';
 
 class Score extends Component {
+
+  shouldComponentUpdate(nextProps) {
+    if (this.props.score !== nextProps.score) {
+      return true
+    }
+    return true
+  }
+
   render() {
-    console.log('Score: this is scoreToAdd ', this.props.scoreToAdd)
+    let newScores
+    if (this.props.scoresToAdd) {
+      newScores = this.props.scoresToAdd.map((score) => <p>{score}</p> )
+    } else {
+      newScores = ''
+    }
     return(
       <div className={this.props.type + '-container'}>
       {this.props.score}
+      { newScores }
       </div>
     )
   }
