8fc66a8af43e5a77b4d4a1fe8ec38e14e22c8e45
kailinc <kailinc@bu.edu>
2017-11-14 10:52:18 -0500

    Renamed gifs for Parrots
    - did this instead of storing the addresses of the slack emojis

--- src/Parrots.js
+++ src/Parrots.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react'
 import './styles/index.css';
 
 class Parrots extends Component {
+  constructor(props) {
+    this.state = {
+      count: 0
+    }
+  }
   addParrot() {
 
   }
-  
+
   render() {
     if (this.props.addNewParrot) {
       parrots.push('newparrot')
     }
+
     return(
       <div>
         <img className={'parrotMarch parrot'} src={require("./img/fast_parrot.gif")} alt='tree'/>
