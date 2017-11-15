d9bcec39ade48cec6869e41bf0a2e8155e025ba6
kailinc <kailinc@bu.edu>
2017-11-15 08:45:26 -0500

    Finished GameEnding Component
    - it will handle win/loses
    - basically a message depending on the game ending
    - there will be a button to start another game

--- src/GameEnding.js
+++ src/GameEnding.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
 import './styles/index.css';
 
-const GameEnding = ({board, onRestart}) => {
-  // var contents = '';
-  // if (board.hasWon()) {
-  //   contents = 'Good Job!';
-  // } else if (board.hasLost()) {
-  //   contents = 'Game Over';
-  // }
-  // if (!contents) {
-  //   return null;
-  // }
-  // return (
-  //   <div className='overlay'>
-  //     <p className='message'>{contents}</p>
-  //     <button className="tryAgain" onClick={onRestart} onTouchEnd={onRestart}>Try again</button>
-  //   </div>
-  // );
-  return null
+const GameEnding = ({ board, onRestart }) => {
+  let contents = '';
+  if (board.win) {
+    contents = 'Good Job!';
+  } else if (board.lose) {
+    contents = 'Game Over';
+  }
+  if (!contents) {
+    return null;
+  }
+  return (
+    <div className='overlay'>
+      <p className='message'>{contents}</p>
+      <button className="tryAgain" onClick={onRestart}>Another One</button>
+    </div>
+  );
 };
 
 export default GameEnding;
