da3bc479e7e2db876a409ec97173d1b3deaa1d06
kailinc <kailinc@bu.edu>
2017-11-20 08:39:00 -0500

    Renamed SlackEmojis to emoji in CSS files
    - to keep things consistent

--- src/SlackEmojis.js
+++ src/SlackEmojis.js
@@ -4,7 +4,7 @@ import './styles/index.css';
 class SlackEmojis extends Component {
   render() {
     let emojiArray = new Array(this.props.numSlackEmojis).fill(' ')
-    let emojis = emojiArray.map((emoji, index) => <img className={'parrot parrotMarch'} src={require(`./img/slackEmoji${index}.gif`)} alt='emoji'/>)
+    let emojis = emojiArray.map((emoji, index) => <img className={'emoji emojiMarch'} src={require(`./img/slackEmoji${index}.gif`)} alt='emoji'/>)
     return(
       <div id="parrots">
         { emojis }
